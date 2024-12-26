6493c288adc0382097abe9bde707962f878a7ef4
Evgenia <124833271+evgy3@users.noreply.github.com>
2024-12-26 14:51:25 +0200

    Add files via upload

---
+++ web project/js/main.js
@@ -0,0 +1,18 @@
+
+const menuBtn = document.querySelector('.menu__btn');
+const menu = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', ()=> {
+    menu.classList.toggle('menu__list--active');
+});
+
+const swiper = new Swiper(".swiper", {
+    effect: "fade",
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+  });
